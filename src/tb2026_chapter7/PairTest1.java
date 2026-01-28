882c38064a2a2ef3f40a91b35d923333c492494e
Le Thanh <thanhlnt@tlu.edu.vn>
2026-01-28 15:53:09 +0700

    changes in pairtest

--- src/tb2026_chapter7/PairTest1.java
+++ src/tb2026_chapter7/PairTest1.java
@@ -2,7 +2,7 @@ package tb2026_chapter7;
 
 public class PairTest1 {
 	public static void main(String[] args) {
-		String[] words = { "Mary", "had", "a", "little", "lamb" };
+		String[] words = { "Java", "programming", "textbook", "2026"};
 		Pair<String> mm = ArrayAlg.minmax(words);
 		System.out.println("min = " + mm.getFirst());
 		System.out.println("max = " + mm.getSecond());
