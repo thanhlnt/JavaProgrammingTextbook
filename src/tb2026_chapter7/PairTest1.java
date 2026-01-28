d934cc02095366fb06f4cc84953b29665438ca30
Le Thanh <thanhlnt@tlu.edu.vn>
2026-01-28 15:22:59 +0700

    first version, chapter 7

---
+++ src/tb2026_chapter7/PairTest1.java
@@ -0,0 +1,10 @@
+package tb2026_chapter7;
+
+public class PairTest1 {
+	public static void main(String[] args) {
+		String[] words = { "Mary", "had", "a", "little", "lamb" };
+		Pair<String> mm = ArrayAlg.minmax(words);
+		System.out.println("min = " + mm.getFirst());
+		System.out.println("max = " + mm.getSecond());
+	}
+}
