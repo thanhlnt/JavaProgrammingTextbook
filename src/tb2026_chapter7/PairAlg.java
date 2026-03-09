84dd3575ac774b7eeef86535b432446d57bdea50
Le Thanh <thanhlnt@tlu.edu.vn>
2026-03-09 15:58:06 +0700

    pairtest3

---
+++ src/tb2026_chapter7/PairAlg.java
@@ -0,0 +1,16 @@
+package tb2026_chapter7;
+
+public class PairAlg {
+	
+	public static boolean hasNulls(Pair<?> p) {
+		return p.getFirst() == null || p.getSecond() == null;
+	}
+	
+	public static void swap(Pair<?> p) { swapHelper(p); }
+	
+	public static <T> void swapHelper(Pair<T> p) {
+		T t = p.getFirst();
+		p.setFirst(p.getSecond());
+		p.setSecond(t);
+	}
+}
